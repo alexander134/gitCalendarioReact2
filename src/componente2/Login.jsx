ab146163f8dd6df1433722184c80b2d4fbcfaa20
alexander134 <alexanderdiaz_134@hotmail.com>
2022-10-20 20:06:06 -0300

    modificación de nav 'Login'

--- src/componente2/Login.jsx
+++ src/componente2/Login.jsx
@@ -3,7 +3,7 @@ import React,{ useState } from 'react'
 
 const Login = (props) => {
   const [registro, setregistro] = useState(true)
-  const [formularioError, setformularioError] = useState({emailT:'Debe agregar un email',passwordT:'Debe agregar una contraseña'})
+  const [formularioError, setformularioError] = useState({emailT:'Debe agregar un email',passwordT:'Debe agregar una contraseña',passwordT2:'Debe ingresar nuevamente la contraseña'})
   const [datos, setdatos] = useState([])
 
 
@@ -13,41 +13,67 @@ const Login = (props) => {
   const procesarDatos = e =>{
     e.preventDefault()
     if(validacionDatos()){
-    
+      console.log("paso validacion");
+    }else{
+      console.log("NO paso validacion");
     }
   }
   const validacionDatos= ()=>{
     let error={}
+    debugger;
     if(Object.keys(datos).length !== 0){
-        if(datos.email===undefined || datos.email ===''){
-            error.email=true
-        }else if(datos.email.trim()===''){
-            error.email=true
+      if(datos.email===undefined || datos.email ==='' || datos.email.trim()===''){
+          error.email=true
+      }else{
+          error.email=false
+      }
+      error.passwordT='Debe agregar una contraseña'
+      if(datos.password===undefined || datos.password ==='' || datos.password.trim()===''){
+          error.password=true
+      }else if(datos.password.length < 6){
+          error.password=true
+          error.passwordT='La contraseña debe ser mayor a 6 caracteres'
+      }else{
+        error.password=false
+      }
+      if(!registro){
+        error.passwordT2='Debe ingresar nuevamente la contraseña'
+        if(datos.password2===undefined || datos.password2 ==='' || datos.password2.trim()===''){
+            error.password2=true
+        }else if(datos.password2.length < 6){
+            error.password2=true
+            error.passwordT2='La contraseña debe ser mayor a 6 caracteres'
+        }else if(datos.password2!==datos.password){
+          error.password=true
+          error.passwordT='Las contraseñas deben ser iguales'
+          error.password2=false
         }else{
-            error.email=false
+          error.password2=false
         }
-        if(datos.password===undefined || datos.password ===''){
-            error.password=true
-        }else if(datos.password.trim()===''){
-            error.password=true
-        }else{
-            error.password=false
-        }
+      }
     }else{
         error.email=true
         error.password=true
     }
     setformularioError({...formularioError,...error})
-    if(!error.nombre && !error.password){
+    if(!error.email && !error.password){
+      if(registro){
         return true
+      }else{
+        if(!error.password2){
+          return true
+        }else{
+          return false
+        }
+      }
     }else{
-        return false
+      return false
     }
-}
+  }
   
   return (
     <div className='mt-5'>
-      <h3 className='text-center'>Acceso o Resgistro de nuevo ususrios</h3>
+      <h3 className='text-center'>{registro ? "Acceso de Ususrio" : "Nuevo Usuario" }</h3>
       <hr/>
       <div className="row justify-content-center">
         <div className="col-12 col-sm-8 col-md-6 col-xl-4">
@@ -56,13 +82,18 @@ const Login = (props) => {
             <input type="email" className="form-control mb-2" placeholder='Ingrese un email' name='email' onChange={(e)=>formulariollenado(e)} />
             { formularioError.password===true && (<div className="invalid-feedbackaa text-danger">{formularioError.passwordT}</div>)} 
             <input type="password" className="form-control mb-2" placeholder='Ingrese password' name='password'  onChange={(e)=>formulariollenado(e)}/>
-            <button className="btn btn-info btn-lg btn-sm btn-block">Registrarse</button>
-            <button className="btn btn-link btn-lg btn-sm btn-block">¿Ya tienes cuenta?</button>
+            { formularioError.password2===true && (<div className="invalid-feedbackaa text-danger">{formularioError.passwordT2}</div>)} 
+            { !registro && (<input type="password" className="form-control mb-2" placeholder='Repita password' name='password2'  onChange={(e)=>formulariollenado(e)}/>)}
+            <button className={`btn btn-lg btn-sm btn-block ${registro ? "btn-info" : "btn-success"  }`}>{registro ? "Ingresar" : "Crear Cuenta" }</button>
+            <button className="btn btn-link btn-lg btn-sm btn-block" onClick={e=>setregistro(!registro)}>
+            {registro ? "¿Ya tienes cuenta?" : "Tengo Cuenta!" }</button>
           </form>
         </div>
       </div>
     </div>
+    
   )
+
 }
 
 export default Login
