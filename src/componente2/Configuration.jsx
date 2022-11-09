f22b95a42de430ec4b2d20850c5b3d7381884d5c
alexander134 <alexanderdiaz_134@hotmail.com>
2022-11-08 23:26:29 -0300

    se desarrolló una funcion que encapsulada para los meses anteriores y siguientes

--- src/componente2/Configuration.jsx
+++ src/componente2/Configuration.jsx
@@ -1,52 +1,79 @@
+import { clear } from '@testing-library/user-event/dist/clear';
 import React, { Fragment } from 'react'
+import { createRef } from 'react';
 
 const Configuration = (props) => {
-var año = 2022;
-var mes = 11;
-const moonLanding = new Date();
 
-  var diasSemana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado','Domingo'];
-  var cantDiaxMes = new Date(moonLanding.getFullYear(), moonLanding.getMonth()+1, 0).getDate();
 
-  const makeNumArr = num => new Array(num).fill("").map((_, i) => {return  {numeroDia:i + 1,diaDSemana:new Date(moonLanding.getFullYear(),moonLanding.getMonth(),i + 1).toLocaleDateString('es-ES', { weekday: 'long' }),fecha:new Date(moonLanding.getFullYear(),moonLanding.getMonth(),i + 1)}});
-  const arrayMes=makeNumArr(cantDiaxMes)
+const tresMeses={
+  mesAnterior:Object(),
+  mesActual:Object(),
+  mesSiguiente:Object()
+}
 
-  function completarDias(flag){
-    let num = true;
-    if(flag==='iniciales'){
-      return diasSemana.map((dia)=> {
-        if(!num){ return undefined}
-        if(dia.toLowerCase()===arrayMes[0].diaDSemana){ 
-          num=false;
-          return undefined
-        }	
-        return dia
-      })
-    }else{
-      return diasSemana.map((dia)=> {
-        if(!num){ return dia}
-        if(dia.toLowerCase()===arrayMes.slice(-1)[0].diaDSemana){
-          num=false;
-          return undefined
+
+const diasSemana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado','Domingo'];
+
+
+
+const funcArrayMesActual = (num) =>new Array(num.cantidadDias).fill(num).map((obj, i) => {
+    return  {numeroDia:i + 1,diaDSemana:new Date(obj.anio,obj.mes,i + 1).toLocaleDateString('es-ES', { weekday: 'long' }),fecha:new Date(obj.anio,obj.mes,i + 1)}
+  })
+
+
+const llenado=(moonLanding=new Date(),anioA=new Date().getFullYear(),mesA=new Date().getMonth())=>{
+
+  tresMeses.mesAnterior={cantidadDias:new Date(anioA, mesA, 0).getDate(),anio:new Date(anioA, mesA, 0).getFullYear(),mes:moonLanding.getMonth()-1,nombreMes:new Date(anioA, mesA, 0).toLocaleDateString('es-ES', { month: 'long' })}
+  tresMeses.mesActual={cantidadDias:new Date(anioA, mesA+1, 0).getDate(),anio:moonLanding.getFullYear(),mes:moonLanding.getMonth(),nombreMes:moonLanding.toLocaleDateString('es-ES', { month: 'long' })}
+  tresMeses.mesSiguiente={cantidadDias:new Date(anioA, mesA+2, 0).getDate(),anio:new Date(anioA, mesA+2, 0).getFullYear(),mes:moonLanding.getMonth()+1,nombreMes:new Date(anioA, mesA+2, 0).toLocaleDateString('es-ES', { month: 'long' })}
+
+  tresMeses.mesActual.arrayDay=funcArrayMesActual(tresMeses.mesActual)
+  tresMeses.mesAnterior.arrayDay=funcArrayMesActual(tresMeses.mesAnterior)
+  tresMeses.mesSiguiente.arrayDay=funcArrayMesActual(tresMeses.mesSiguiente)
+  
+  let flag=true;
+  tresMeses.mesActual.arrayDay=[...tresMeses.mesActual.arrayDay,
+  ...tresMeses.mesSiguiente.arrayDay.map((elem)=>{
+        if(flag){
+          if(elem.diaDSemana=="domingo"){
+            flag=false
+          }
+          return elem
+        }return undefined
+    }).filter(element => {
+      return element !== undefined;
+    })]
+
+    flag=true;
+    tresMeses.mesActual.arrayDay=[...tresMeses.mesAnterior.arrayDay.reverse().map((elem)=>{
+      if(flag){
+        if(elem.diaDSemana=="lunes"){
+          flag=false
         }
-        if(!num){ return dia}
-        return undefined
-      })
-    }	
-  }
-  const diaCompleados= {
-    diasIniciales:completarDias('iniciales').filter(element => {
-      return element !== undefined;
-    }),
-    diasFinales:completarDias().filter(element => {
-      return element !== undefined;
-    })
-  }
+        return elem
+      }return undefined
+  }).filter(element => {
+    return element !== undefined;
+  }).reverse(),...tresMeses.mesActual.arrayDay]
+}
+
+console.clear()
+llenado()
 
   return (
     <div className='container-fluid'>
+
       <div className="row">
-        <div class="col p-1  text-center">
+        <div className="col-8 offset-2 align-self-center text-center p-1">
+            <div className="card">
+              <div className="card-body p-0">
+                <h5 className="card-title text-secondary m-0">2022</h5>
+              </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col p-1  text-center">
           <div className="card bg-secondary">
             <div className="card-body p-1">
               <h5 className="card-title m-1">Oct</h5>
@@ -60,7 +87,7 @@ const moonLanding = new Date();
             </div>
           </div>
         </div>
-      <div class="col p-1  text-center">
+      <div className="col p-1  text-center">
         <div className="card bg-secondary">
           <div className="card-body p-1">
             <h5 className="card-title m-1">Dic</h5>
@@ -70,27 +97,15 @@ const moonLanding = new Date();
       <div className="w-100"></div>
         {
           diasSemana.map((dia,index)=>(
-            <div className="col text-center p-1" key={index}>{dia}</div>
+            <div className="col text-center p-1" key={index}>
+            <h5 className="card-title text-success m-1">{dia}</h5>
+              </div>
           ))
         }
         <div className="w-100"></div>
+        
         {
-          diaCompleados.diasIniciales.map((e,i)=>(
-            <div className="col text-center p-1" key={i}>
-              <div className="card">
-                <div className="card-body p-1">
-                  <h5 className="card-title text-secondary m-1">0</h5>
-                  <p className="card-text text-muted m-1"></p>
-                  <div className='d-flex justify-content-around'>
-                  <button type='button' className="btn btn-sm btn-dark p-1">Ver</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))
-        }
-        {
-          arrayMes.map((dia,index)=>(
+          tresMeses.mesActual.arrayDay.map((dia,index)=>(
             dia.diaDSemana ==='domingo'? 
               (<Fragment key={index}>
                 <div className="col text-center p-1">
@@ -120,21 +135,6 @@ const moonLanding = new Date();
                   </div>
                 </div>
               ) 
-          ))
-        }
-        {
-          diaCompleados.diasFinales.map((e,i)=>(
-            <div className="col text-center p-1" key={i}>
-              <div className="card">
-                <div className="card-body p-1">
-                  <h5 className="card-title text-secondary m-1">0</h5>
-                  <p className="card-text text-muted m-1"></p>
-                  <div className='d-flex justify-content-around'>
-                  <button type='button' className="btn btn-sm btn-dark p-1">Ver</button>
-                  </div>
-                </div>
-              </div>
-            </div>
           ))
         }
         
