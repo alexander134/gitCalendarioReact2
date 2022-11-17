54c1667e7ed9340ad6c82da827b8c49ec1e58528
alexander134 <alexanderdiaz_134@hotmail.com>
2022-11-17 12:38:51 -0300

    corrección de chefas termio

--- src/componente2/Configuration.jsx
+++ src/componente2/Configuration.jsx
@@ -7,9 +7,9 @@ const diasSemana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sába
 const funcArrayMesActual = (num) =>new Array(num.cantidadDias).fill(num).map((obj, i) => {
     return  {
       numeroDia:i + 1,
-      diaDSemana:new Date(obj.anio,obj.mes===-1?0:obj.mes===12?11:obj.mes,i + 1).toLocaleDateString('es-ES', { weekday: 'long' }),
-      nombreMes:new Date(obj.anio,obj.mes===-1?0:obj.mes===12?11:obj.mes,i + 1).toLocaleDateString('es-ES', { month: 'long' }),
-      fecha:new Date(obj.anio,obj.mes===-1?0:obj.mes===12?11:obj.mes,i + 1)}
+      diaDSemana:new Date(obj.anio,obj.mes===-1?11:obj.mes===12?0:obj.mes,i + 1).toLocaleDateString('es-ES', { weekday: 'long' }),
+      nombreMes:new Date(obj.anio,obj.mes===-1?11:obj.mes===12?0:obj.mes,i + 1).toLocaleDateString('es-ES', { month: 'long' }),
+      fecha:new Date(obj.anio,obj.mes===-1?11:obj.mes===12?0:obj.mes,i + 1)}
   })
 
 
